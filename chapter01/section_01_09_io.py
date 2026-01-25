41720adac71bd64828a8d6fb99413cca85706b35
seejokim1 <sjkim1@anu.ac.kr>
2026-01-25 11:30:12 +0900

    Create section_01_09_io.py

---
+++ chapter01/section_01_09_io.py
@@ -0,0 +1,9 @@
+# =========================================================
+# Section (1.9) 입출력
+# =========================================================
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hello,", name)
+print("Age:", age)
