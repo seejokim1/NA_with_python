f7d5cf5c4e31c18eaf054a051320b9a49679a097
seejokim1 <sjkim1@anu.ac.kr>
2026-01-25 11:06:20 +0900

    Update section_01_01_features.py

--- chapter01/section_01_01_features.py
+++ chapter01/section_01_01_features.py
@@ -1,1 +1,8 @@
+"""
+(1.1) 파이썬의 특징
+Numerical Analysis with Python
+"""
+
+print("Python is an interpreted, high-level programming language.")
+print("It supports multiple programming paradigms.")
 
