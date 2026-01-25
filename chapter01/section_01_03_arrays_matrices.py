5ff8873140885035291feb8c09c50c51b359700f
seejokim1 <sjkim1@anu.ac.kr>
2026-01-25 11:25:13 +0900

    Create section_01_03_arrays_matrices.py

---
+++ chapter01/section_01_03_arrays_matrices.py
@@ -0,0 +1,22 @@
+# =========================================================
+# Section (1.3) 배열(Array)과 행렬(Matrix)
+# =========================================================
+
+# Python list
+a = [1, 2, 3, 4]
+b = [5, 6, 7, 8]
+
+print(a[0])
+print(a[-1])
+
+# NumPy array
+import numpy as np
+
+A = np.array([1, 2, 3])
+B = np.array([4, 5, 6])
+
+print(A + B)
+
+# 2D array
+M = np.array([[1, 2], [3, 4]])
+print(M)
