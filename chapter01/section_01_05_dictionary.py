aab8fe4a3df89fdd67a235451f835b1bd06195ef
seejokim1 <sjkim1@anu.ac.kr>
2026-01-25 11:26:54 +0900

    Create section_01_05_dictionary.py

---
+++ chapter01/section_01_05_dictionary.py
@@ -0,0 +1,17 @@
+# =========================================================
+# Section (1.5) 사전(Dictionary)
+# =========================================================
+
+student = {
+    "name": "Alice",
+    "age": 25,
+    "major": "Engineering"
+}
+
+print(student["name"])
+
+student["age"] = 26
+student["grade"] = "A"
+del student["major"]
+
+print(student)
